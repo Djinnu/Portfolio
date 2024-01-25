d8ce7790efaf343f2023ae42e9143815d723cae7
Djinnu <dzinnu.hunt@gmail.com>
2024-01-25 05:20:14 +0200

    changed action attribute

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -38,7 +38,7 @@ const Contact = () => {
                 </ul>
             </div>
             <div className='contact-form'>
-                <form method="POST" action="14be38905c0493b8dc24d63533a3c329">
+                <form method="POST" action="https://formsubmit.co/14be38905c0493b8dc24d63533a3c329">
                     <input ref={input1Ref} name='name' type='text' placeholder='name' required/>
                     <input ref={input2Ref} name='email' type='email' placeholder='email' required/>
                     <textarea ref={textRef} name='message' placeholder='message'></textarea>
